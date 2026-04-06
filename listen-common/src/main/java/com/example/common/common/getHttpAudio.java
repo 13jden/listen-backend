7d02bb2af2f49bdc57ef881e4022ec2a4756281a
13jdden <13jdenden@gmail.com>
2026-04-06 21:11:47 +0800

    fix:修改资源返回路由

--- listen-common/src/main/java/com/example/common/common/getHttpAudio.java
+++ listen-common/src/main/java/com/example/common/common/getHttpAudio.java
@@ -4,10 +4,9 @@ public class getHttpAudio {
     public static String getAudioUrl(String localFilePath) {
 
         if(localFilePath!=null){
-//            String baseUrl = "https://aidatech.cn/";  // Nginx 映射的 HTTP 路径
+//            String baseUrl = "https://audiotest.top/";  // Nginx 映射的 HTTP 路径
             String baseUrl = "http://115.190.53.97/";
-            String fileName = localFilePath.replace("usr/local/listen/temp", "");  // 提取文件名
-            return baseUrl + fileName;
+            return baseUrl + localFilePath;
         }
         return null;
     }
